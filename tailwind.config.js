5acbf31ef6703bac88339823576568cb957bef7c
piyush06071992 <rana.rpiyush.piyush826@gmail.com>
2026-04-19 13:07:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
+  content: ["./**/*.html"],
+  theme: { extend: {} },
   plugins: [],
 }
